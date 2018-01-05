39022dfd718c9c8d1553e8aec6bc0afbf19a88ab
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-05 19:26:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,9 +88,9 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
     holiday_hash.collect do |season,holiday|
-     holiday.collect do |holiday,supplies|
+     holiday.compact do |holiday,supplies|
       if supplies.include?("BBQ")
-         holiday.compact
+         holiday
       end
        #binding.pry
      end
