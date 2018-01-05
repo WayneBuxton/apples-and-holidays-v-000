f059e6bf18677f39dc3855f5a87195e24e2fd4ab
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-05 18:45:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,6 @@ def all_holidays_with_bbq(holiday_hash)
      holiday.collect do |holiday,supplies|
       if supplies.include?("BBQ")
          holiday.to_sym
-       else
 
       end
        #binding.pry
