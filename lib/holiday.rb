87f559aa762067f65ae895523050ac6247590982
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-05 19:01:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,7 @@ def all_holidays_with_bbq(holiday_hash)
        #binding.pry
      end
 
-   end.flatten.to_sym
+   end.flatten.join
 
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
